358e4f5dcae0f915e8cdcccb37ba77ca6fcb7656
TunaKiet <tuankietpro167@gmail.com>
2025-05-17 16:12:36 +0700

    first commit

---
+++ views/common/adminHeader.php
@@ -0,0 +1,27 @@
+<div class="page-title-area">
+    <div class="row align-items-center">
+        <div class="col-sm-6">
+            <div class="nav-btn pull-left">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="breadcrumbs-area clearfix">
+                <h4 class="page-title pull-left">Dashboard</h4>
+                <!-- <ul class="breadcrumbs pull-left">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>Đơn hàng</span></li>
+                </ul> -->
+            </div>
+        </div>
+        <div class="col-sm-6 clearfix">
+            <div class="user-profile pull-right">
+                <img class="avatar user-thumb"
+                    src="<?php echo BASE_PATH . '/app/public/assets/images/author/avatar.png' ?>" alt="avatar" />
+                <h4 class="user-name dropdown-toggle" data-toggle="dropdown">
+                    Admin</i>
+                </h4>
+            </div>
+        </div>
+    </div>
+</div>
